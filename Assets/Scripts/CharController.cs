360ad8701bb9a75ba06792b6252280c3081ee4d2
Carole Chao <carolechao@hotmail.com>
2021-02-22 14:46:55 -0500

    Prototype 2: Modify Environment (Add terrain + trees) and Character Controller (Player)

---
+++ Assets/Scripts/CharController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Set the character movement speed
+
+public class CharController : MonoBehaviour
+{
+    public float movementSpeed = 40f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hMovement = Input.GetAxis("Horizontal") * movementSpeed / 2;
+        float vMovement = Input.GetAxis("Vertical") * movementSpeed;
+
+        transform.Translate(new Vector3(hMovement, 0, vMovement) * Time.deltaTime);
+    }
+}
